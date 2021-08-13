f8e64def8a0c3a7f126a977a81bc95295465ba8e
odaki <takuo6942@gmail.com>
2021-08-13 20:41:15 +0900

    Firebase Cloud Messaging対応

--- app/src/main/java/pw/copan/MainActivity.kt
+++ app/src/main/java/pw/copan/MainActivity.kt
@@ -1,18 +1,30 @@
 package pw.copan
 
+import android.content.ContentValues
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import androidx.appcompat.app.AppCompatActivity
+import android.widget.Toast
+
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.messaging.FirebaseMessaging
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
+
 
     private lateinit var wv: WebView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
         wv = findViewById<View>(R.id.wv) as WebView
         wv.settings.javaScriptEnabled = true // JavaScriptの有効化
@@ -24,5 +36,20 @@ class MainActivity : AppCompatActivity() {
             }
         })
         wv.loadUrl("http://copan.pw/")
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w(TAG, "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            // Get new FCM registration token
+            val token = task.result
+
+            // Log and toast
+            val msg = getString(R.string.msg_token_fmt, token)
+            Log.d(TAG, msg)
+            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+        })
     }
 }
