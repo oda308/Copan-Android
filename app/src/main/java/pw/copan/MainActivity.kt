e9688bf5c9b8ee137a98b6e7c157c44a9821d1b4
odaki <takuo6942@gmail.com>
2021-08-13 13:39:05 +0900

    家計簿アプリCopan、Android版ソースコードの追加

---
+++ app/src/main/java/pw/copan/MainActivity.kt
@@ -0,0 +1,26 @@
+package pw.copan
+
+import android.os.Bundle
+import android.view.View
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var wv: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        wv = findViewById<View>(R.id.wv) as WebView
+        // リンク先もwebviewで表示するための処理
+        wv.setWebViewClient(object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
+                return false
+            }
+        })
+        wv.loadUrl("http://copan.pw/")
+    }
+}
